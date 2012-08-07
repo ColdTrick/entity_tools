4308ce256947a729c24c0caa33d0b43bda06f7e9
Jerome Bakker <jeabakker@coldtrick.com>
2012-08-07 16:51:37 +0200

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,32 @@
+<?php
+$language = array (
+  'entity_tools:created' => 'Aangemaakt',
+  'entity_tools:owner' => 'Eigenaar',
+  'entity_tools:container' => 'Groep / Gebruiker',
+  'entity_tools:error:check_edit_access' => 'Je mag geen content beheren',
+  'entity_tools:error:unsupported_subtype' => 'Je probeert een niet ondersteund type content the beheren (%s)',
+  'entity_tools:menu:user_hover' => 'Content beheer',
+  'entity_tools:menu:owner_block' => 'Content beheer',
+  'entity_tools:page:owner:title' => 'Beheer %s\'s: %s',
+  'entity_tools:forms:owner_listing:description' => 'Hier kun je een aantal attributen beheren van je content. De kolom Aangemaakt stelt je in staat de creatiedatum aan te passen. De kolom Eigenaar stelt je in staat om het item over te dragen aan een van je vrienden. De laatste kolom (Groep / Gebruiker) verteld waar het item zich bevind, of in een groep of in je persoonlijke overzicht en stelt je in staat dit aan te passen.',
+  'entity_tools:forms:owner_listing:disclaimer' => 'Houdt er rekening mee dat als je content plaatst in een groep met een *, de huidige eigenaar geen lid is van die groep en dus niet meer bij het item kan.',
+  'entity_tools:settings:edit_access' => 'Wie kan de eigenschappen van de content aanpassen',
+  'entity_tools:settings:edit_access:admin' => 'Alleen site beheerders',
+  'entity_tools:settings:edit_access:user' => 'Alle gebruikers',
+  'entity_tools:notify:transfer_owner:subject' => 'Er is een %s aan je overgedragen',
+  'entity_tools:notify:transfer_owner:message' => 'Beste %s,
+
+%s heeft een %s met de titel \'%s\' aan je overgedragen.
+
+Je kunt het hier bekijken: %s',
+  'entity_tools:dropdown:label:current_value' => 'Huidige waarde',
+  'entity_tools:dropdown:label:owner' => 'Huidige eigenaar',
+  'entity_tools:dropdown:label:myself' => 'Mijzelf',
+  'entity_tools:dropdown:label:friends' => 'Mijn vrienden',
+  'entity_tools:dropdown:label:owner_groups' => 'Groepen van de eigenaar',
+  'entity_tools:dropdown:label:my_groups' => 'Mijn groepen',
+  'entity_tools:action:update_entities:error:input' => 'Niet alle vereiste velden zijn ingevuld, probeer het nognaals',
+  'entity_tools:action:update_entities:error:not_all' => 'Niet alle item konden worden bijgewerkt (%s van de %s gelukt)',
+  'entity_tools:action:update_entities:success' => 'Succesvol %s items aangepast.',
+);
+add_translation("nl", $language);
