cb6d442814774d81dfa412788493aaa309172bf6
Jerome Bakker <jeabakker@coldtrick.com>
2012-08-07 15:45:28 +0200

    fixed: css issue in chrome

--- views/default/entity_tools/css/site.php
+++ views/default/entity_tools/css/site.php
@@ -4,6 +4,7 @@
 #entity-tools-listing-table .elgg-input-datetime,
 #entity-tools-listing-table .elgg-input-dropdown {
 	width: 140px;
+	white-space: nowrap;
 }
 
 #entity-tools-listing-table td:first-child {
